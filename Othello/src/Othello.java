8a6ed96286d29f468ab333be0d3e7eee1ee3a8f2
Pizza-beep307 <younes.chelda@gmail.com>
2025-10-14 12:34:57 +0200

    Tab changed from String[][] to char[][]

--- Othello/src/Othello.java
+++ Othello/src/Othello.java
@@ -56,18 +56,18 @@ class Othello{
 		return start;
 	}
 	
-	String[][] boardList(int lines) {
-		String[][] tab = new String[lines][lines];
+	char[][] boardList(int lines) {
+		char[][] tab = new char[lines][lines];
 		int firstMidTab = lines/2 - 1;
 		int secMidTab = lines/2;
 		for (int i = 0; i < tab.length; i++) {
 			for (int j = 0; j < tab.length; j++) {
 				if ((i == firstMidTab && j == firstMidTab) || (i == secMidTab && j == secMidTab)) {
-					tab[i][j] = " x ";
+					tab[i][j] = 'x';
 				} else if ((i == secMidTab && j == firstMidTab) || (i == firstMidTab && j == secMidTab)) {
-					tab[i][j] = " o ";
+					tab[i][j] = 'o';
 				} else {
-					tab[i][j] = "   ";
+					tab[i][j] = ' ';
 				}
 			}
 		}
@@ -78,34 +78,33 @@ class Othello{
 	 * It displays to the console the board of the game
 	 * @param lines An integer which is the number of lines (from nbLines())
 	 */
-	void displayGame(String[][] tab) {
-		int i;
+	void displayGame(char[][] tab) {
 		int j;
-		System.out.print("  ");
-		for (i = 0; i < tab.length; i++) {
-			if (i>=10) {
+		System.out.print("   ");
+		for (int i = 0; i < tab.length; i++) {
+			if ( i>= 10) {
 				System.out.print("  "+i);
 			} else {
 				System.out.print("   "+i);
 			}
 		}
-		i = 0;
-		for (i = 0; i < tab.length; i++) {
+		for (int k = 0; k < tab.length; k++) {
 			System.out.println();
-			if (i >= 10) {
-				System.out.print(i + " ");
+			if (k >= 10) {
+				System.out.print(k + " ");
 			} else {
-				System.out.print(i + "  ");
+				System.out.print(k + "  ");
 			}
 			for (j = 0; j < tab.length; j++) {
-				System.out.print("|" + tab[i][j]);
+				System.out.print(" | " + tab[k][j]);
+			
 			}
-			System.out.print("|");
+			System.out.print(" |");
 		}
 	}
 
 	
-	int[][] indiceTab(String[][] tab, String x) {
+	int[][] indiceTab(char[][] tab, char x) {
 		int i = 0;
 		int j = 0;
 		int n = 0;
@@ -124,6 +123,31 @@ class Othello{
 		}
 		return indTab;
 	}
+	
+	int[][] validCases(char[][] tab, char player) {
+		char opponent;
+		if (player == 'o') {
+			opponent = 'x';
+		} else { 
+			oppenent = 'o';
+		}
+		//The 2D list validMove associate every single position around our case 
+		//		topLeft, top, topRight,
+		//		left, case, right
+		//		botLeft, bot, botRight	
+		int[][] validMove = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+		for (int i = 0; i < tab.length; i++) {
+			for (int j = 0; j < tab.length; j++) {
+				if (tab[i][j] == ' ') {
+					boolean validCase = false; // if we encounter just a space its not a valid case yet
+					for (int k = 0; k < validMove.length; k++) {
+						int dx = validMove[k][0];
+						int dy = validMove[k][1];
+						int x = i + dx;
+						int y = j + dy;
+						if (x >= 
+					}
+
 
 	
 }
